da208aa9019b919777ed35408dadee97637cb8fb
Vitalii Romantsov <vitaliyromantsov@gmail.com>
2025-10-01 22:12:33 +0200

    clear commit

--- demo-fixtures/types/value/example.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from './value.fixture';
-
-test('user profile page should show the correct username', async ({ page, testUser }) => {
-    // `testUser` is now available as an object!
-    await page.goto(`/profile/${testUser.username}`);
-    // ... assertions using testUser.username
-});
